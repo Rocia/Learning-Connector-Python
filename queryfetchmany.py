ca6796daed5a8df4a556d0a87800d03fcd820960
Rocia <rocia.fernandes@gmail.com>
2017-08-23 16:48:19 +0530

    updated fetch many from the DB

--- queryfetchmany.py
+++ queryfetchmany.py
@@ -17,4 +17,20 @@ def iter_row(cursor, size=10):
             yield row
 
 	
-	
+def query_with_fetchmany():
+    try:
+        dbconfig = read_db_config()
+        conn = MySQLConnection(**dbconfig)
+        cursor = conn.cursor()
+ 
+        cursor.execute("SELECT * FROM books")
+ 
+        for row in iter_row(cursor, 10):
+            print(row)
+ 
+    except Error as e:
+        print(e)
+ 
+    finally:
+        cursor.close()
+        conn.close()			
